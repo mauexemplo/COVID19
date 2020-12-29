46c3f5f59b6a336d623035e6bbaac1ee8ea930ee
Daniel <fonseca.daniel@gmail.com>
2020-12-28 23:44:22 -0300

    ibge.R: Added initial data and routines for using the Localidades API

--- ibge.R
+++ ibge.R
@@ -1,16 +1,62 @@
 require( dplyr )
 require( httr )
 require( readxl )
+require( jsonlite )
 
-ibge_rms_url <- "https://geoftp.ibge.gov.br/organizacao_do_territorio/estrutura_territorial/municipios_por_regioes_metropolitanas/Situacao_2020a2029/Composicao_RMs_RIDEs_AglomUrbanas_2020_06_30.xlsx"
+ibge_rms_url <-
+  "https://geoftp.ibge.gov.br/organizacao_do_territorio/estrutura_territorial/municipios_por_regioes_metropolitanas/Situacao_2020a2029/Composicao_RMs_RIDEs_AglomUrbanas_2020_06_30.xlsx"
 ibge_rms_ext <- ".xlsx"
 
-ibge_drb_url <- "https://geoftp.ibge.gov.br/organizacao_do_territorio/divisao_regional/divisao_regional_do_brasil/divisao_regional_do_brasil_em_regioes_geograficas_2017/tabelas/regioes_geograficas_composicao_por_municipios_2017_20180911.xlsx"
+ibge_drb_url <-
+  "https://geoftp.ibge.gov.br/organizacao_do_territorio/divisao_regional/divisao_regional_do_brasil/divisao_regional_do_brasil_em_regioes_geograficas_2017/tabelas/regioes_geograficas_composicao_por_municipios_2017_20180911.xlsx"
 ibge_drb_ext <- ".xlsx"
 
-ibge_dtb_url <- "https://geoftp.ibge.gov.br/organizacao_do_territorio/estrutura_territorial/divisao_territorial/2019/DTB_2019_v2.zip"
+ibge_dtb_url <-
+  "https://geoftp.ibge.gov.br/organizacao_do_territorio/estrutura_territorial/divisao_territorial/2019/DTB_2019_v2.zip"
 ibge_dtb_file <- "RELATORIO_DTB_BRASIL_MUNICIPIO.xls"
 ibge_dtb_ext <- ".xls"
+
+ibge_api_host <- "https://servicodados.ibge.gov.br"
+ibge_loc_api_path <- "api/v1/localidades"
+ibge_api_endpoints <- data.frame(
+  name = c( "Região", "UF", "Município", "Distrito", "Subdistrito",
+            "Região Intermediária", "Região Imediata", "Mesorregião",
+            "Microrregião" ),
+  group = factor( c( "Divisão Político-Administrativa",
+             "Divisão Político-Administrativa",
+             "Divisão Político-Administrativa",
+             "Divisão Político-Administrativa",
+             "Divisão Político-Administrativa",
+             "Regiões Geográficas",
+             "Regiões Geográficas",
+             "Mesorregiões e Microrregiões",
+             "Mesorregiões e Microrregiões" ) ),
+  order_in_group = c( 1, 2, 3, 4, 5, 1, 2, 1, 2 ),
+  endpoint = c( "regioes", "estados", "municipios", "distritos",
+                "subdistritos", "regioes-intermediarias", "regioes-imediatas",
+                "mesorregioes", "microrregioes" ),
+  filters = I( list( `regioes` = c(),
+                     `estados` = c( "regioes" ),
+                     `municipios` = c( "estados", "mesorregioes",
+                                       "microrregioes", "regioes-imediatas",
+                                       "regioes-intermediarias", "regioes" ),
+                     `distritos` = c( "estados", "mesorregioes",
+                                      "microrregioes", "municipios",
+                                      "regioes-imediatas",
+                                      "regioes-intermediarias", "regioes" ),
+                     `subdistritos` = c( "distrito", "estados", "mesorregioes",
+                                         "microrregioes", "municipios",
+                                         "regioes" ),
+                     `regioes-intermediarias` = c( "estados", "regioes" ),
+                     `regioes-imediatas` = c( "estados",
+                                              "regioes-intermediarias",
+                                              "regioes" ),
+                     `mesorregioes` = c( "estados", "regioes" ),
+                     `microrregioes` = c( "estados", "mesorregioes", "regioes" )
+                     )
+               )
+  )
+
 
 # Download, unzip and import the DTB file
 loadIBGEDTB <- function( path = ibge_dtb_url )
@@ -91,15 +137,14 @@ addRegionID <- function( rm_data = NULL )
   return ( rm_data )
 }
 
-# Builds and returns a named vector of all states (COD_UF) in the RM file
-# with their parent regions (GRANDE_REG)
-getRegionTable <- function( rm_data = NULL )
+# Builds and returns a named vector of all states (COD_UF) in the RM file with
+# their parent regions (GRANDE_REG)
+getRegionMembership <- function( rm_data = NULL )
 {
   if ( is.null( rm_data ) )
     rm_data <- loadIBGERMs()
 
   regions <- rm_data %>% dplyr::distinct( COD_UF, GRANDE_REG ) %>% 
-    dplyr::select( COD_UF, GRANDE_REG ) %>%
     # AC (12) and MS (50) do not appear in any RMs, need to add manually
     dplyr::bind_rows(
       data.frame( COD_UF = c( 12, 50 ),
@@ -107,6 +152,23 @@ getRegionTable <- function( rm_data = NULL )
     dplyr::arrange( COD_UF )
 
   return ( with( regions, setNames( GRANDE_REG, COD_UF ) ) )
+}
+
+# Builds and returns a named vector of all states (COD_UF) in the RM file with
+# their corresponding abbreviation (SIGLA_UF)
+# NOTE: Manually adds entries for states with no RM
+getStateAbbr <- function( rm_data = NULL )
+{
+  if ( is.null( rm_data ) )
+    rm_data <- loadIBGERMs()
+
+  abbr <- rm_data %>% dplyr::distinct( COD_UF, SIGLA_UF ) %>% 
+    # AC (12) and MS (50) do not appear in any RMs, need to add manually
+    dplyr::bind_rows( data.frame( COD_UF = c( 12, 50 ),
+                                  SIGLA_UF = c( "AC", "MS" ) ) ) %>% 
+    dplyr::filter( !is.na( SIGLA_UF ) ) %>% dplyr::arrange( COD_UF )
+
+  return ( with( abbr, setNames( SIGLA_UF, COD_UF ) ) )
 }
 
 # Add the RM ID and NOME to the DTB table (requires casting the join key as
@@ -130,7 +192,7 @@ addRMtoDTB <- function( rm_data = NULL, dtb_data = NULL )
 
   # After the join, cities not belonging to any RM are also not assigned
   # to a region (GRANDE_REG), this uses UF from DTB to assign
-  regions <- getRegionTable( rm_data )
+  regions <- getRegionMembership( rm_data )
   full_data$GRANDE_REG <- unname( regions[ full_data$UF ] )
   
   return ( full_data )
@@ -142,6 +204,7 @@ addRMtoDTB <- function( rm_data = NULL, dtb_data = NULL )
 getCities <- function( dtb_data = NULL, rm_data = NULL, drb_data = NULL )
 {
   flatCities <- addRMID( addRegionID( addRMtoDTB( rm_data, dtb_data ) ) )
+  abbrs <- getStateAbbr( flatCities )
   flatCities <- flatCities %>%
     # Rename vars to keep with convention, and cast numbers to integer
     dplyr::mutate( `Região` = RID, `Nome_Região` = GRANDE_REG,
@@ -155,6 +218,7 @@ getCities <- function( dtb_data = NULL, rm_data = NULL, drb_data = NULL )
                    `Mesorregião Geográfica` =
                      as.integer( paste0( UF, `Mesorregião Geográfica` ) ),
                    UF = as.integer( UF ),
+                   Sigla_UF = unname( abbrs[ as.character( UF ) ] ),
                    `Região Geográfica Intermediária` =
                      as.integer( `Região Geográfica Intermediária` ),
                    `Região Geográfica Imediata` =
@@ -174,8 +238,46 @@ getCities <- function( dtb_data = NULL, rm_data = NULL, drb_data = NULL )
                    `Região Geográfica Intermediária`,
                    `Nome_Região Geográfica Intermediária`,
                    `Região Metropolitana`, `Nome_Região Metropolitana`,
-                   `Tipo_Região Metropolitana`, `UF`, `Nome_UF`, `Região`,
-                   `Nome_Região` )
+                   `Tipo_Região Metropolitana`, `UF`, `Nome_UF`, Sigla_UF,
+                   `Região`, `Nome_Região` )
   
   return ( flatCities )
+}
+
+# Provides full details about an individual city, or all cities if unspecified
+getCityByID <- function( id = NULL, ... )
+{
+  url <- paste0( ibge_api_host,
+                 ibge_api_endpoints[
+                   ibge_api_endpoints$name == "Município", "endpoint" ] )
+  if( !is.null( id ) )
+  {
+    url <- paste( url, id, sep = "/" )
+  }
+
+  
+
+}
+
+# Generic retrieval using the Localidade API
+getLocalidade <- function( type, id = NULL, filter = NULL, ... )
+{
+  #TODO: Validate type and filter with match.arg
+  
+  url <- paste( ibge_api_host, ibge_loc_api_path, sep = "/" )
+  
+  if( length( id ) > 1 )
+    id <- paste( id, collapse = "|" )
+  
+  if( !is.null( filter ) )
+  { url <- paste( url, filter, id, type, sep = "/" ) }
+  else if( !is.null( id ) )
+  { url <- paste( url, type, id, sep = "/" ) }
+  else
+  { url <- paste( url, type, sep = "/" ) }
+
+  warning( url )
+  
+  #TODO: Add user agent, validate response
+  return( jsonlite::fromJSON( url ) )
 }
